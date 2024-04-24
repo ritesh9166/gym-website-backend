b214cd6a6da8b6cfac549e270fa4803af9d40071
Ritesh Kumar <117093847+ritesh9166@users.noreply.github.com>
2024-04-24 20:43:07 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const collection = require("./mongodb");
+const contact = require("./contactdb");
+let alert = require("alert");
+require("dotenv").config()
+
+
+
+const bodyParser = require('body-parser')
+const admin = require('./route')
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + "/public/"));
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(admin)
+
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "localhost";
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://${HOST}:${PORT}`);
+});
